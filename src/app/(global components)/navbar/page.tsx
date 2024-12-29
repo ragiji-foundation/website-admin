978200a22adfd7951601964859917c9fd46ae906
Manish Kumar <manish.jsx@gmail.com>
2024-12-29 14:43:10 +0530

    build passed

--- src/app/(global components)/navbar/page.tsx
+++ src/app/(global components)/navbar/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import HeaderMenu  from '@/components/Pages/HeaderMenu'
-import EditLinksModal from '@/components/Edit/EditLinksModal'
+
 
 export default function NavbarPage() {
   return (
     <>
        <HeaderMenu/>
-       <EditLinksModal/>
+  
     </>
  
 
